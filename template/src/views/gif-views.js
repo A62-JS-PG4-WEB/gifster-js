b4fb0c9af11294e11609df6469fe25eb14df7660
Martina Beleva <martina_bel@abv.bg>
2024-07-13 10:58:40 +0300

    adding CSS for gif details, upload gifs page

--- template/src/views/gif-views.js
+++ template/src/views/gif-views.js
@@ -5,21 +5,21 @@ export const toGifSimple = (gif) => `
 <div class="gif-simple">
   <h1>${gif.title}</h1>
   
-  <img class="detailed-func" src="${gif.images.fixed_height.url}"><br>
+  <img class="detailed-func" src="${gif.images.fixed_height.url}"alt="GIF Image"><br>
 </div>
 `;
 
 export const toGifDetails = (gif) => `
 <div class="gif-detailed-info">
-  <div class="poster">
-    <img src="${gif.images.fixed_height.url}">
-  </div>
-  <div class="gif-info">
-    <p>${gif.title}</p>
+<div class="gif-info">
+  <p>${gif.title}</p>
     <p>user: ${gif.username}</p>
     <p>Source: ${gif.source ? `<a href="${gif.source}" target="_blank">${gif.source}</a>` : 'Source not provided.'}</p>
-    
-    
+ </div>    
+
+  <div class="poster">
+    <img src="${gif.images.fixed_height.url}" alt="GIF Image">
+ 
   </div>
 </div>
 `;
