e3ef3d49dbb06f2c564674c2ba3c779ddaff5dc0
Lyuboslava1 <lyuboslava.ivanova@abv.bg>
2024-07-14 17:35:25 +0300

    gif-views updated function toGifDetails/fixed source link

--- template/src/data/favorites-gifs.js
+++ template/src/data/favorites-gifs.js
@@ -2,7 +2,7 @@
 
 // export const addFavorite = (gifId) => {
 //   if (favorites.find(id => id === gifId)) {
-//     // Movie has already been added to favorites
+//     // Gif has already been added to favorites
 //     return;
 //   }
 
