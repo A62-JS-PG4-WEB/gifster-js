8963b4968d46ea925de9823beba9cd5e5d528388
Martina Beleva <martina_bel@abv.bg>
2024-07-15 21:55:05 +0300

    spinner adding

--- template/src/requests/loading-event.js
+++ template/src/requests/loading-event.js
@@ -1,18 +1,18 @@
 export const showLoadingSpinner = () => {
     const spinner = document.getElementById('loading-spinner');
     const overlay = document.createElement('div');
-  overlay.classList.add('overlay');
-  document.body.appendChild(overlay);
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
     spinner.style.display = 'block';
-    
+
 };
 
 export const hideLoadingSpinner = () => {
     const spinner = document.getElementById('loading-spinner');
     const overlay = document.querySelector('.overlay');
-    
+
     spinner.style.display = 'none';
     if (overlay) {
         document.body.removeChild(overlay);
-      }
+    }
 };
