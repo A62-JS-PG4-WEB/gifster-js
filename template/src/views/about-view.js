9bef643209871f2b34dc44129baf4af8947531f5
Martina Beleva <martina_bel@abv.bg>
2024-07-16 13:30:04 +0300

    working on about page

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -1,12 +1,24 @@
 export const toAboutView = () => `
 <div id="about">
-    <h1> About our GIFster app</h1>
-     <hr>
-     <div class="about-content">
-    <h2>Authors: Martina, Ivo, Lyuba</h2>
-    <img src="https://media.giphy.com/media/3xz2BDFvxop2BfAQoM/giphy.gif" alt="Giphy Image" class="about-image">
-    </h2>
+<div id="about">
+    <h1>About our GIFster app</h1>
+    <hr>
+     
+    <div class="about-content">
+        <div class="author">
+            <h2> Martina</h2>
+            <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjZ3ZDl6cWx1YWF3YXlzNThjbnA3ZjdqM2NyaGdidmpoZ2J2NzkzZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QULa93AhcmTveZZyC9/giphy.gif" alt="Giphy Image" class="about-image">
+        </div>
+        <div class="author">
+            <h2>Ivo</h2>
+            <img src="https://media.giphy.com/media/l4FGuhL4U2WyjdkaY/giphy.gif" alt="Giphy Image" class="about-image">
+        </div>
+        <div class="author">
+            <h2>Lyuba</h2>
+            <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbzd1c25mc3k3b3hmOW41eG90ZW53dnU5cWVscmZoNThobWlpMm81dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZkqzMBDDQoWpG/giphy.gif" alt="Giphy Image" class="about-image">
+        </div>
+    </div>
     <h2 class="about-date">Date: 2024</h2>
-  </div>
 </div>
+
 `;
